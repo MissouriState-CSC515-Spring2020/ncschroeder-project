c3af0cf0df7ad7445dcebd75f75987524d7c21c2
Nicholas Schroeder <nschroeder131@outlook.com>
2020-02-22 23:20:58 -0600

    Added router
    Made application similar to checkpoint 2

---
+++ src/views/CityPictures.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="CityPictures">
+		<h1 class="heading">City Pictures</h1>
+		
+		<router-link to="/GoldenSpiral">
+			<img src="../assets/city1.jpg" class="city_image">
+		</router-link>
+
+		<router-link to="/GoldenSpiral">
+			<img src="../assets/city2.jpg" class="city_image">
+		</router-link>
+
+		<router-link to="/GoldenSpiral">
+			<img src="../assets/city3.jpg" class="city_image">
+		</router-link>
+
+		<router-link to="/GoldenSpiral">
+			<img src="../assets/city4.jpg" class="city_image">
+		</router-link>
+
+		<router-link to="/GoldenSpiral">
+			<img src="../assets/city5.jpg" class="city_image">
+		</router-link>
+
+		<router-link to="/GoldenSpiral">
+			<img src="../assets/city6.jpg" class="city_image">
+		</router-link>
+		
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+	.city_image {
+		width: 200px
+	}
+</style>
---
+++ src/views/GoldenSpiral.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="heading">Golden Spiral</h1>
+        <div>
+            <img src="../assets/golden_spiral.jpg" width=400>
+            <p>This rectangle is approximately 1.618 times wider than 
+                it is tall. 1 to 1.618 is approximately the golden ratio
+                so this is a golden rectangle and the spiral inside it 
+                is a golden spiral
+            </p>
+        </div>
+    </div>
+</template>
